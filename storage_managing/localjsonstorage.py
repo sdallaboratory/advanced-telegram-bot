e45cb3d28353f46766aa0d2c079b6b1024ab5c23
Minish144 <varlamow.col@yahoo.com>
2020-08-26 19:25:26 +0300

    remove methods

--- storage_managing/localjsonstorage.py
+++ storage_managing/localjsonstorage.py
@@ -4,7 +4,6 @@ from utils.datafiltering import DataFiltering
 import os
 import json
 
-
 class LocalJSONStorage(Storage):
     def __init__(self, storage_folder: str):
         self.__folder = storage_folder + '/'
@@ -15,6 +14,11 @@ class LocalJSONStorage(Storage):
             print(f'{folder} was successfully initialized!')
         else:
             print(f'{folder} already exists!')
+
+    def __write_data_to_json(self, file_name: str, data: list) -> None:
+        with open(file_name, 'w') as data_file:
+            json.dump(data, data_file)
+            data_file.close()
 
     #---------------------------------------------------------------------------
     # Getting methods
@@ -65,9 +69,7 @@ class LocalJSONStorage(Storage):
             
             data.append(doc)
 
-            with open(self.__folder + collection + '.json', 'w') as data_file:
-                json.dump(data, data_file)
-                data_file.close()
+            self.__write_data_to_json(self.__folder + collection + '.json', data)
 
         except:
             raise StorageException('Failed to insert a doc to collection')
@@ -81,9 +83,7 @@ class LocalJSONStorage(Storage):
             
             data += docs
 
-            with open(self.__folder + collection + '.json', 'w') as data_file:
-                json.dump(data, data_file)
-                data_file.close()
+            self.__write_data_to_json(self.__folder + collection + '.json', data)
 
         except:
             raise StorageException('Failed to insert a doc to collection')
@@ -93,16 +93,57 @@ class LocalJSONStorage(Storage):
     #---------------------------------------------------------------------------
     
     def remove_one_doc_by_column(self, collection: str, column: str, value: str) -> None:
-        pass
+        self.remove_one_doc_by_dict(collection=collection,
+                                    doc={column: value})
 
     def remove_one_doc_by_dict(self, collection: str, doc: dict) -> None:
-        pass
+        try:
+            count = 0
+            data = self.get_data(collection=collection)
+            for dic in data:
+                try:
+                    if doc.items() <= dic.items():
+                        count += 1
+                        data.remove(dic)
+                        break
+                except:
+                    continue
+            
+            self.__write_data_to_json(self.__folder + collection + '.json', data)
+
+            if count == 0:
+                print('0 docs were removed from collection!')
+            else:
+                print(f'Succesfully removed a doc from {collection}!')
+
+        except:
+            raise StorageException('Failed to remove a doc from collection!')
 
     def remove_many_docs_by_column(self, collection: str, column: str, value: str):
-        pass
+        self.remove_many_docs_by_dict(collection=collection,
+                                    doc={column: value})
 
     def remove_many_docs_by_dict(self, collection: str, doc: dict) -> None:
-        pass
+        try:
+            count = 0
+            data = self.get_data(collection=collection)
+            for dic in data.copy():
+                try:
+                    if doc.items() <= dic.items():
+                        data.remove(dic)
+                        count += 1
+                except:
+                    continue
+            
+            self.__write_data_to_json(self.__folder + collection + '.json', data)
+
+            if count == 0:
+                print('0 docs were removed from collection!')
+            else:
+                print(f'Succesfully removed {count} docs with {doc} from {collection}!')
+
+        except:
+            raise StorageException('Failed to remove a doc from collection!')
 
     #---------------------------------------------------------------------------
     # Update methods
