9f9248e7f675a10ed17f9163d57238844fbb791a
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-09-16 18:38:25 +0300

    added packages __init__

---
+++ components/__init__.py
@@ -0,0 +1,5 @@
+from .role_managing.roleauth import RoleAuth
+from .state_managing.statemanager import StateManager
+from .user_meta.usermetastorage import UserMetaStorage
+from .locales.localemanager import LocaleManager
+from .logs.botlogger import BotLogger
---
+++ telegrambot.py
@@ -0,0 +1,6 @@
+import components.RoleAuth
+
+class TelegramBot:
+    def __init__(self):
+        self.role_auth = RoleAuth
+        self.state_manager = 
