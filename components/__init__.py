b656d0d8fbf29d0ee47058b545f4c21a1c48edb4
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-09-21 01:07:13 +0300

    added components to init

--- components/__init__.py
+++ components/__init__.py
@@ -3,3 +3,6 @@ from .state_managing.statemanager import StateManager
 from .user_meta.usermetastorage import UserMetaStorage
 from .locales.localemanager import LocaleManager
 from .logs.botlogger import BotLogger
+from .storage_managing.storage import Storage
+from .storage_managing.localjsonstorage import LocalJSONStorage
+from .storage_managing.mongodbstorage import MongoDBStorage
